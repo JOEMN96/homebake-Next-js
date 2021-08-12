0b2a87582ecbd814dbddf55941ba814bff1cd07f
Joe Mn <aruljoe37@gmail.com>
2021-08-12 21:30:08 +0530

    next js webpack fix

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -6,3 +6,16 @@ module.exports = {
     domains: ["localhost"],
   },
 };
+
+module.exports = {
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      // don't resolve 'fs' module on the client to prevent this error on build --> Error: Can't resolve 'fs'
+      config.resolve.fallback = {
+        fs: false,
+      };
+    }
+
+    return config;
+  },
+};
