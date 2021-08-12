2ab2cde9dd6961dacdcd6ffb36db1700fa6ab175
Joe Mn <aruljoe37@gmail.com>
2021-08-12 23:31:19 +0530

    remove congig

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,14 +3,4 @@ module.exports = {
   images: {
     domains: ["res.cloudinary.com"],
   },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      // don't resolve 'fs' module on the client to prevent this error on build --> Error: Can't resolve 'fs'
-      config.resolve.fallback = {
-        fs: false,
-      };
-    }
-
-    return config;
-  },
 };
