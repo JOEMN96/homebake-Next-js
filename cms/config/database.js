e7d2602a09b17834638a6394bcb0015ea0a0ebfc
Joe Mn <aruljoe37@gmail.com>
2021-08-14 17:06:32 +0530

    added mongodb atlas

--- cms/config/database.js
+++ cms/config/database.js
@@ -1,19 +1,17 @@
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
+  defaultConnection: "default",
   connections: {
     default: {
-      connector: 'mongoose',
+      connector: "mongoose",
       settings: {
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        srv: env.bool('DATABASE_SRV', false),
-        port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME', 'hombakecms'),
-        username: env('DATABASE_USERNAME', null),
-        password: env('DATABASE_PASSWORD', null),
+        uri: env("DATABASE_URI"),
+        srv: env.bool("DATABASE_SRV", true),
+        port: env.int("DATABASE_PORT", 27017),
+        database: env("DATABASE_NAME"),
       },
       options: {
-        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
-        ssl: env.bool('DATABASE_SSL', false),
+        authenticationDatabase: env("AUTHENTICATION_DATABASE", null),
+        ssl: env.bool("DATABASE_SSL", true),
       },
     },
   },
